0af509e3e2d8684aaf86db8bc3240826e3bb031e
Iionccub365 <101848989+Iionccub365@users.noreply.github.com>
2022-03-29 17:29:44 -0600

    Add files via upload

---
+++ LionBrowser 1.0.3/LionBrowser 1.0.3/Form1.Designer.vb
@@ -0,0 +1,239 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.b14 = New System.Windows.Forms.Button()
+        Me.b15 = New System.Windows.Forms.Button()
+        Me.b11 = New System.Windows.Forms.Button()
+        Me.b13 = New System.Windows.Forms.Button()
+        Me.b12 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.Tab1 = New System.Windows.Forms.Button()
+        Me.Tab2 = New System.Windows.Forms.Button()
+        Me.b2 = New System.Windows.Forms.Button()
+        Me.b22 = New System.Windows.Forms.Button()
+        Me.b23 = New System.Windows.Forms.Button()
+        Me.b24 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.b25 = New System.Windows.Forms.Button()
+        Me.WebBrowser2 = New System.Windows.Forms.WebBrowser()
+        Me.SuspendLayout()
+        '
+        'b14
+        '
+        Me.b14.Location = New System.Drawing.Point(255, 12)
+        Me.b14.Name = "b14"
+        Me.b14.Size = New System.Drawing.Size(75, 23)
+        Me.b14.TabIndex = 0
+        Me.b14.Text = "Home"
+        Me.b14.UseVisualStyleBackColor = True
+        '
+        'b15
+        '
+        Me.b15.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.b15.Location = New System.Drawing.Point(713, 12)
+        Me.b15.Name = "b15"
+        Me.b15.Size = New System.Drawing.Size(75, 23)
+        Me.b15.TabIndex = 1
+        Me.b15.Text = "GO!"
+        Me.b15.UseVisualStyleBackColor = True
+        '
+        'b11
+        '
+        Me.b11.Location = New System.Drawing.Point(12, 13)
+        Me.b11.Name = "b11"
+        Me.b11.Size = New System.Drawing.Size(75, 23)
+        Me.b11.TabIndex = 2
+        Me.b11.Text = "Back"
+        Me.b11.UseVisualStyleBackColor = True
+        '
+        'b13
+        '
+        Me.b13.Location = New System.Drawing.Point(174, 12)
+        Me.b13.Name = "b13"
+        Me.b13.Size = New System.Drawing.Size(75, 23)
+        Me.b13.TabIndex = 3
+        Me.b13.Text = "Refresh"
+        Me.b13.UseVisualStyleBackColor = True
+        '
+        'b12
+        '
+        Me.b12.Location = New System.Drawing.Point(93, 12)
+        Me.b12.Name = "b12"
+        Me.b12.Size = New System.Drawing.Size(75, 23)
+        Me.b12.TabIndex = 4
+        Me.b12.Text = "Forward"
+        Me.b12.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox1.Location = New System.Drawing.Point(337, 13)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(370, 20)
+        Me.TextBox1.TabIndex = 5
+        Me.TextBox1.Text = "Type/Copy and paste URL"
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.WebBrowser1.Location = New System.Drawing.Point(13, 70)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.Size = New System.Drawing.Size(775, 368)
+        Me.WebBrowser1.TabIndex = 6
+        '
+        'Tab1
+        '
+        Me.Tab1.Location = New System.Drawing.Point(12, 41)
+        Me.Tab1.Name = "Tab1"
+        Me.Tab1.Size = New System.Drawing.Size(370, 23)
+        Me.Tab1.TabIndex = 7
+        Me.Tab1.Text = "Tab1"
+        Me.Tab1.UseVisualStyleBackColor = True
+        '
+        'Tab2
+        '
+        Me.Tab2.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Tab2.Location = New System.Drawing.Point(388, 41)
+        Me.Tab2.Name = "Tab2"
+        Me.Tab2.Size = New System.Drawing.Size(400, 23)
+        Me.Tab2.TabIndex = 8
+        Me.Tab2.Text = "Tab2"
+        Me.Tab2.UseVisualStyleBackColor = True
+        '
+        'b2
+        '
+        Me.b2.Location = New System.Drawing.Point(12, 12)
+        Me.b2.Name = "b2"
+        Me.b2.Size = New System.Drawing.Size(75, 23)
+        Me.b2.TabIndex = 9
+        Me.b2.Text = "Back"
+        Me.b2.UseVisualStyleBackColor = True
+        '
+        'b22
+        '
+        Me.b22.Location = New System.Drawing.Point(93, 13)
+        Me.b22.Name = "b22"
+        Me.b22.Size = New System.Drawing.Size(75, 23)
+        Me.b22.TabIndex = 10
+        Me.b22.Text = "Forward"
+        Me.b22.UseVisualStyleBackColor = True
+        '
+        'b23
+        '
+        Me.b23.Location = New System.Drawing.Point(174, 13)
+        Me.b23.Name = "b23"
+        Me.b23.Size = New System.Drawing.Size(75, 23)
+        Me.b23.TabIndex = 11
+        Me.b23.Text = "Refresh"
+        Me.b23.UseVisualStyleBackColor = True
+        '
+        'b24
+        '
+        Me.b24.Location = New System.Drawing.Point(255, 13)
+        Me.b24.Name = "b24"
+        Me.b24.Size = New System.Drawing.Size(75, 23)
+        Me.b24.TabIndex = 12
+        Me.b24.Text = "Home"
+        Me.b24.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox2.Location = New System.Drawing.Point(336, 12)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(370, 20)
+        Me.TextBox2.TabIndex = 13
+        '
+        'b25
+        '
+        Me.b25.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.b25.Location = New System.Drawing.Point(713, 13)
+        Me.b25.Name = "b25"
+        Me.b25.Size = New System.Drawing.Size(75, 23)
+        Me.b25.TabIndex = 14
+        Me.b25.Text = "GO!"
+        Me.b25.UseVisualStyleBackColor = True
+        '
+        'WebBrowser2
+        '
+        Me.WebBrowser2.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.WebBrowser2.Location = New System.Drawing.Point(13, 70)
+        Me.WebBrowser2.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser2.Name = "WebBrowser2"
+        Me.WebBrowser2.Size = New System.Drawing.Size(775, 368)
+        Me.WebBrowser2.TabIndex = 15
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.WebBrowser2)
+        Me.Controls.Add(Me.b25)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.b24)
+        Me.Controls.Add(Me.b23)
+        Me.Controls.Add(Me.b22)
+        Me.Controls.Add(Me.b2)
+        Me.Controls.Add(Me.Tab2)
+        Me.Controls.Add(Me.Tab1)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.b12)
+        Me.Controls.Add(Me.b13)
+        Me.Controls.Add(Me.b11)
+        Me.Controls.Add(Me.b15)
+        Me.Controls.Add(Me.b14)
+        Me.Name = "Form1"
+        Me.Text = "LionBrowser 1.0.3"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents b14 As Button
+    Friend WithEvents b15 As Button
+    Friend WithEvents b11 As Button
+    Friend WithEvents b13 As Button
+    Friend WithEvents b12 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents WebBrowser1 As WebBrowser
+    Friend WithEvents Tab1 As Button
+    Friend WithEvents Tab2 As Button
+    Friend WithEvents b2 As Button
+    Friend WithEvents b22 As Button
+    Friend WithEvents b23 As Button
+    Friend WithEvents b24 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents b25 As Button
+    Friend WithEvents WebBrowser2 As WebBrowser
+End Class
---
+++ LionBrowser 1.0.3/LionBrowser 1.0.3/Form1.vb
@@ -0,0 +1,81 @@
+﻿Public Class Form1
+    Private b21 As Object
+
+    Private Sub b11_Click(sender As Object, e As EventArgs) Handles b11.Click
+        WebBrowser1.GoBack()
+    End Sub
+
+    Private Sub b12_Click(sender As Object, e As EventArgs) Handles b12.Click
+        WebBrowser1.GoForward()
+    End Sub
+
+    Private Sub b13_Click(sender As Object, e As EventArgs) Handles b13.Click
+        WebBrowser1.Refresh()
+    End Sub
+
+    Private Sub b14_Click(sender As Object, e As EventArgs) Handles b14.Click
+        WebBrowser1.Navigate("sites.google.com/view/lionbrowser/home")
+    End Sub
+
+    Private Sub b15_Click(sender As Object, e As EventArgs) Handles b15.Click
+        WebBrowser1.Navigate(TextBox1.Text)
+    End Sub
+
+    Private Sub TextBox1_Click(sender As Object, e As EventArgs) Handles TextBox1.Click
+        TextBox1.Text = ""
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        WebBrowser1.Navigate("sites.google.com/view/lionbrowser/home")
+        WebBrowser1.BringToFront()
+        TextBox1.BringToFront()
+        b11.BringToFront()
+        b12.BringToFront()
+        b13.BringToFront()
+        b14.BringToFront()
+        b15.BringToFront()
+    End Sub
+
+    Private Sub Tab1_Click(sender As Object, e As EventArgs) Handles Tab1.Click
+        WebBrowser1.BringToFront()
+        TextBox1.BringToFront()
+        b11.BringToFront()
+        b12.BringToFront()
+        b13.BringToFront()
+        b14.BringToFront()
+        b15.BringToFront()
+    End Sub
+    Private Sub b2_Click(sender As Object, e As EventArgs) Handles b2.Click
+        WebBrowser2.GoBack()
+    End Sub
+
+    Private Sub b22_Click(sender As Object, e As EventArgs) Handles b22.Click
+        WebBrowser2.GoForward()
+    End Sub
+
+    Private Sub b23_Click(sender As Object, e As EventArgs) Handles b23.Click
+        WebBrowser2.Refresh()
+    End Sub
+
+    Private Sub b24_Click(sender As Object, e As EventArgs) Handles b24.Click
+        WebBrowser2.Navigate("sites.google.com/view/lionbrowser/home")
+    End Sub
+
+    Private Sub b25_Click(sender As Object, e As EventArgs) Handles b25.Click
+        WebBrowser2.Navigate(TextBox2.Text)
+    End Sub
+
+    Private Sub TextBox2_Click(sender As Object, e As EventArgs) Handles TextBox2.Click
+        TextBox2.Text = ""
+    End Sub
+
+    Private Sub Tab2_Click(sender As Object, e As EventArgs) Handles Tab2.Click
+        WebBrowser2.BringToFront()
+        TextBox2.BringToFront()
+        b2.BringToFront()
+        b22.BringToFront()
+        b23.BringToFront()
+        b24.BringToFront()
+        b25.BringToFront()
+    End Sub
+End Class
