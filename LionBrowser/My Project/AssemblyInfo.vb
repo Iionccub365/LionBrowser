ab67512a7959eb8918089bbab76d59804d15d7d8
Ryan Russon <IT@rrusson.com>
2022-04-12 18:11:35 -0400

    Basic project clean-up; added icon

---
+++ LionBrowser/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("LionBrowser")>
+<Assembly: AssemblyDescription("It's a web browser!")>
+<Assembly: AssemblyCompany("LionelSoft")>
+<Assembly: AssemblyProduct("LionBrowser 1.0.3")>
+<Assembly: AssemblyCopyright("Copyright © LionelSoft 2022")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("23283d5a-6601-4e56-b0b2-ba0cddb47d29")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.3.0")>
+<Assembly: AssemblyFileVersion("1.0.3.0")>
